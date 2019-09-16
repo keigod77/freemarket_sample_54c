465db74b0f371086ac08136a4acee3207d22c924
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-09-16 20:40:47 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.assets.css_compressor = :sass  
+  config.assets.css_compressor = :sass 
+  config.serve_static_assets = false 
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
