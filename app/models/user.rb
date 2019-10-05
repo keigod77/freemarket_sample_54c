92cbe319a619d78b0e79045e62d2ed7f5f921db9
Keigo Dohmae <keigod0707@gmail.com>
2019-10-05 18:23:33 +0900

    userモデルaddressモデル　テスト記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :addresses
-  accepts_nested_attributes_for :addresses
+
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, inclusion: { in: %w( @ ) }
+  validates :password, presence: true, length: { in: 7..128 } #英字と数字両方含むも追加
+  validates :password_confirmation, presence: true, confirmation: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday_year, presence: true
+  validates :birthday_month, presence: true
+  validates :birthday_day, presence: true
+  validates :phone_number, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    id                    {"1"}
+    nickname              {"yamada"}
+    email                 {"yamada@gmail.com"}
+    password              {"yamada000"}
+    password_confirmation {"yamada000"}
+    first_name            {"太郎"}
+    last_name             {"山田"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"ヤマダ"}
+    birthday_year         {"2000"}
+    birthday_month        {"7"}
+    birthday_day          {"10"}
+    phone_number          {"1234567"}
   end
 end
