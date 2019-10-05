362930a96713e7a33bfedf0a20f2fde63ff29d46
Keigo Dohmae <keigod0707@gmail.com>
2019-10-05 14:18:09 +0900

    データ登録機能+rspec導入

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,6 +1,6 @@
 class SignupController < ApplicationController
-  before_action :no_layout_action, except: :create
-  before_action :prefectures , only: :address
+  layout :false
+  # before_action :prefectures , only: :address
 
   def registration
     @user = User.new
@@ -11,14 +11,13 @@ class SignupController < ApplicationController
     session[:email]  =user_params[:email]
     session[:password] = user_params[:password]
     session[:password_confirmation] = user_params[:password_confirmation]
-    session[:family_name_knaji] = user_params[:family_name_knaji]
-    session[:first_name_kanji] = user_params[:first_name_kanji]
-    session[:family_name_kana] = user_params[:family_name_kana]
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
     session[:first_name_kana] = user_params[:first_name_kana]
     session[:birthday_year] = user_params[:birthday_year]
     session[:birthday_month] = user_params[:birthday_month]
     session[:birthday_day] = user_params[:birthday_day]
-    session[:user_id] = @user.id
     @user = User.new
   end
 
@@ -30,14 +29,14 @@ class SignupController < ApplicationController
   def credit_card
     session[:postal_code] = addresses_params[:postal_code]
     session[:prefectures] = addresses_params[:prefectures]
-    session[:city] = addresses_params[:city]
-    session[:address] = addresses_params[:address]
-    session[:building] = addresses_params[:building]
+    session[:city]        = addresses_params[:city]
+    session[:address]     = addresses_params[:address]
+    session[:building]    = addresses_params[:building]
     @user = User.new
   end
 
   def complete
-
+    sign_in User.find(session[:id]) unless user_signed_in?
   end
 
   def create
@@ -46,36 +45,42 @@ class SignupController < ApplicationController
       email: session[:email],
       password: session[:password],
       password_confirmation: session[:password_confirmation],
-      family_name_knaji: session[:family_name_knaji],
-      first_name_kanji: session[:first_name_kanji],
-      family_name_kana: session[:family_name_kana],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
       first_name_kana: session[:first_name_kana],
       birthday_year: session[:birthday_year],
-      birthday_month: session[:],
-      birthday_day,
-      phone_number,
+      birthday_month: session[:birthday_month],
+      birthday_day: session[:birthday_day],
+      phone_number: session[:phone_number],
     )
-    @address = Address.create(
-
+    session[:id] = @user.id
+    @address = Address.new(
+      user_id: session[:id],
+      postal_code: session[:postal_code],
+      prefectures: session[:prefectures],
+      city: session[:city],
+      address: session[:address],
+      building: session[:building],
     )
-    
-    redirect_to complete_signup_index_path
+    if @address.save
+      redirect_to complete_signup_index_path
+    else
+      User.find(session[:id]).destroy
+      render '/signup/registration'
+    end
   end
 
   private
-  def no_layout_action
-    render layout: false
-  end
-
   def user_params
     params.require(:user).permit(
       :nickname, 
       :email, 
       :password, 
       :password_confirmation, 
-      :family_name_knaji, 
-      :first_name_kanji, 
-      :family_name_kana, 
+      :last_name, 
+      :first_name, 
+      :last_name_kana, 
       :first_name_kana, 
       :birthday_year,
       :birthday_month,
