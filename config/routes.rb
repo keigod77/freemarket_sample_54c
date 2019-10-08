a154727f1e70f6af0f3a47825e8625753e1d2d1d
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-10-08 20:18:14 +0900

    fix: routes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  get 'mypage', to: 'mypage#top'
-  get 'mypage/profile', to: 'mypage#profile'
+  resource :mypage do
+    collection do
+      get :top
+      get :profile
+    end
+  end
+
   resources :home, only: [:show, :index]
 
 
