8f512e479b1e56e8b3a04b50448858109e720f7a
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-10-23 23:37:11 +0900

    テスト用enum作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,17 @@
 class Category < ApplicationRecord
+  acts_as_tree
   enum category: {
     レディース:1,メンズ:2,ベビー・キッズ:3,インテリア・住まい・小物:4,
     本・音楽・ゲーム:5,おもちゃ・ホビー・グッズ:6,コスメ・香水・美容:7,
     家電・スマホ・カメラ:8,スポーツ・レジャー:9,ハンドメイド:10,チケット:11,
     自動車・オートバイ:12,その他:13
+  }
+  #テスト用コード
+  enum childrencategory: {
+    トップス:1,ジャケットアウター:2,パンツ:3,スカート:4
+  }
+
+  enum grandchildcategory: {
+    テスト:1, アウター:2
   }
 end
