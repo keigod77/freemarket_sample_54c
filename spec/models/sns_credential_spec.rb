0561738905b7d8b52a2ba72047236ae6d26116ac
Keigo Dohmae <keigod0707@gmail.com>
2019-10-25 16:23:38 +0900

    sns_credentialsのテスト記述

--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+
+    let!(:user) { create(:user, id: 1)}
+    let!(:sns) { build(:sns_credential)}
+
+    context '登録可' do
+
+      it "is valid with provider, uid, user_id" do
+        expect(sns).to be_valid
+      end
+
+    end
+
+    context '登録不可' do
+
+      it "uidが無い" do
+        sns[:uid] = nil
+        sns.valid?
+        expect(sns.errors[:uid]).to include("can't be blank")
+      end
+
+      it "providerが無い" do
+        sns[:provider] = nil
+        sns.valid?
+        expect(sns.errors[:provider]).to include("can't be blank")
+      end
+
+    end
+  end
 end
