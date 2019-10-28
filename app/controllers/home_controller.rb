d901d4b18c6de1418d177336e1e5f9f1162c0ca7
Keigo Dohmae <keigod0707@gmail.com>
2019-10-28 14:56:44 +0900

    %thカテゴリー部分を記述

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,10 @@ class HomeController < ApplicationController
     @user = User.find(@item.user_id)
     # 複数枚画像を取得する際はwhereに変更する
     @image = Image.find_by(item_id: @item.id)
+    #一気に取得する書き方ありそう。時間あれば
+    @grandchild = Category.find(@item.category_id)
+    @child = Category.find(@grandchild.parent_id)
+    @parent = Category.find(@child.parent_id)
   end
 
   def logout
