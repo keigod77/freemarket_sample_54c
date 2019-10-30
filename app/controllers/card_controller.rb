2abdd2e13c51e4b49350ab8ea91773223e4b266d
Keigo Dohmae <keigod0707@gmail.com>
2019-10-30 09:46:35 +0900

    コントローラ記述

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,33 @@
 class CardController < ApplicationController
-  def new
+  require "payjp"
+
+  def new#カード登録
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "index" if card.present?
+  end
+
+  def create#payjpとcardのデータ作成
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else#https://pay.jp/docs/api/?ruby
+      customer = Payjp::Customer.create(
+        description: 'test',
+        email: current_user.email,
+        card: params['payjp-token'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to action: "index"
+      else
+        redirect_to action: "create"
+      end
+    end
   end
 
   def show
+
   end
 end
