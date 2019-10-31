27f3d6ab1bafbd061111096fd43eccb237a466b9
Keigo Dohmae <keigod0707@gmail.com>
2019-10-31 15:41:09 +0900

    購入画面view実装

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -1,4 +1,6 @@
 class BuyController < ApplicationController
+  require 'payjp'
+  before_action :set_card
 
   def show
     @postal = current_user.addresses[0].getPostalCode
@@ -6,6 +8,17 @@ class BuyController < ApplicationController
     @fullname = current_user.getFullname
     @item = Item.find(params[:id])
     @shipping = @item.getShippingCharge
+
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @card_information = customer.cards.retrieve(@card.card_id)
+    binding.pry
+  end
+
+
+  private
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
 
 end
