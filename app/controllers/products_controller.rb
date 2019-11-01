956f3fb7378dc96e8666e7ce686f4017988933ea
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-11-01 21:44:32 +0900

    コントローラ設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+    # 複数枚画像を取得する際はwhereに変更する
+    @image = Image.find_by(item_id: @item.id)
+    #child, parentは@grandchild.parentで取得可能
+    @grandchild = Category.find(@item.category_id)
+  end
+
+
   private
   
   def item_params
