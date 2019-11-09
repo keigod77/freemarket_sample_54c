0d86a77d6205f98773b0c57047c3e6c116efdb97
code-of-sugar <kennbi.ryo3233@gmail.com>
2019-11-09 14:07:03 +0900

    テスト設定　修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,15 +5,15 @@ FactoryBot.define do
     name{"ルイヴィトンの長財布"}
     description{"新品未使用です"}
     category_id {3}
-    state {1}
-    shipping_charge{1}
-    delivery_method{1}
+    state {"新品、未使用"}
+    shipping_charge{"送料込み（出品者負担）"}
+    delivery_method{"未定"}
     region{"北海道"}
-    days_to_delivery {1}
+    days_to_delivery {"１〜２日で発送"}
     price {"333"}
     exhibision_state {0}
     
     association :user, factory: :user
-    association :category
+    association :category, factory: :category
   end
 end 
