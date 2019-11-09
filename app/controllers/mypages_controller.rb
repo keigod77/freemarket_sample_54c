44e7e5d837531a01872b3c3e425f26c3065e10e6
Keigo Dohmae <keigod0707@gmail.com>
2019-11-09 14:51:51 +0900

    mypage/addcard変更

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -14,5 +14,14 @@ class MypagesController < ApplicationController
   end
 
   def add_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_information = customer.cards.retrieve(@card.card_id)
+    end
+
   end
 end
